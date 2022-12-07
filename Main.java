2800250dad1cfe932516c65229c781174200a473
Sujay Golla <119337637+SujayGolla@users.noreply.github.com>
2022-12-07 15:33:27 +0000

    idk

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 class Main extends JFrame{
   public static void main(String[] args) {
     MyFrame frame = new MyFrame("Township");
-    frame.setVisible(true); 
-    frame.setSize(600,600);
+  frame.setVisible(true); 
+  frame.setSize(1000,500);
   }
 }
--- MyFrame.java
+++ MyFrame.java
@@ -11,17 +11,23 @@ import java.awt.*;
     public MyFrame(String title){
       super(title);
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      img = new ImageIcon("Township.jpg");
+      img = new ImageIcon("simtown_mainMenu.jpg");
       playImg = new ImageIcon("play.png");
       btn = new JButton (playImg);
+      btn.setBounds(450,350,100,50);
+      
+      btn.setBorder(null);
+      btn.setBorderPainted(false);
+      btn.setContentAreaFilled(false);
+      btn.setOpaque(false);
       
       ctn = getContentPane();
-      ctn.setLayout(new FlowLayout());
+      ctn.setLayout(null);
       ctn.add(btn);
     }	
   
 	public void paint (Graphics g){
 	  super.paint(g);
-    g.drawImage(img.getImage(),0,10, 600, 600, this);
+    g.drawImage(img.getImage(),0,10, 1000, 500, this);
   }
 }
