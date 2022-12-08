eac7e1ad5f6b92a6c72fed047731e90a894a8714
Sujay Golla <119337637+SujayGolla@users.noreply.github.com>
2022-12-08 16:26:29 +0000

    idk

--- Homepage.java
+++ Homepage.java
@@ -2,34 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Homepage extends JPanel {
-    private JPanel bg;
-    private JPanel bottom;
-    private JPanel top;
-    public Homepage(){
-        bg = new JPanel();
-        bottom = new JPanel();
-        top = new JPanel();
+  private JPanel bg;
+  private JPanel bottom;
+  private JPanel top;
+  public Homepage(){
+      bg = new JPanel();
+      bottom = new JPanel();
+      top = new JPanel();
 
-        this.setLayout(new BorderLayout());
+      this.setLayout(new BorderLayout());
 
-        this.add(top, BorderLayout.NORTH);
-        top.setLayout(new BorderLayout());
-        top.setBackground(Color.DARK_GRAY);
-        top.add(new Label("Third"), BorderLayout.EAST);
-        top.add(new Label("Second"), BorderLayout.CENTER);
-        top.add(new Label("First"), BorderLayout.WEST);
+      this.add(top, BorderLayout.NORTH);
+      top.setLayout(new BorderLayout());
+      top.setBackground(Color.DARK_GRAY);
+      top.add(new Label("Third"), BorderLayout.EAST);
+      top.add(new Label("Second"), BorderLayout.CENTER);
+      top.add(new Label("First"), BorderLayout.WEST);
 
-        this.add(bg, BorderLayout.CENTER);
-        bg.setLayout(new GridLayout());
-        bg.setBackground(Color.GRAY);
-        bg.add(new JLabel(new ImageIcon("Township.jpg")));
+      this.add(bg, BorderLayout.CENTER);
+      bg.setLayout(new GridLayout());
+      bg.setBackground(Color.GRAY);
+      bg.add(new JLabel(new ImageIcon("Township.jpg")));
 
-        this.add(bottom, BorderLayout.SOUTH);
-        bottom.setLayout(null);
-        bottom.setBackground(Color.BLACK);
-    }
+      this.add(bottom, BorderLayout.SOUTH);
+      bottom.setLayout(null);
+      bottom.setBackground(Color.BLACK);
+  }
 
-    public void paintComponent(Graphics g){
-     super.paintComponent(g);
-   }
+  public void paintComponent(Graphics g){
+   super.paintComponent(g);
+ }
 }
