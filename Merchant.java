00879af09d8e60081ad147444557c0a7ab141f9f
Akaren S <99822737+AkarenSutharsan@users.noreply.github.com>
2023-01-11 19:10:56 +0000

    jan 11-2

--- Merchant.java
+++ Merchant.java
@@ -15,13 +15,41 @@ public class Merchant extends JPanel  {
   private String[] items;
   private ShopItem item;
   private int cnt;
+  private JPanel top;
+  private JButton back;
 
   public Merchant() {
+    this.setLayout(new BorderLayout());
+    
     random = (int) (Math.random()*8);
     items = new String[] {"Wheat", "Carrot", "Corn", "Tomatoes", "Rice", "Apples", "Strawberry", "Cotton"};
+    
     item = ShopItem.getShopItem(items[random]);
     cnt = 0;
+    displaySellableItem(item, 50, 50);
+
+    random = (int) (Math.random()*8);
+    item = ShopItem.getShopItem(items[random]);
+    displaySellableItem(item, 50, 50);
     
+    back = new JButton(new ImageIcon("back.png"));
+    back.setBorderPainted(false);
+    back.setContentAreaFilled(false);
+    back.setOpaque(false);
+    back.setFocusable(false);
+    //back.addActionListener(this);
+    
+    
+    top = new JPanel(new GridLayout(1, 5));
+        top.setBackground(Color.GRAY);
+        JLabel title = new JLabel("    Merchant");
+        title.setFont(new Font("Times New Roman", Font.BOLD, 24));
+        top.add(back);
+        top.add(new JLabel(""));
+        top.add(title);
+        top.add(new JLabel(""));
+        top.add(new JLabel(""));
+    this.add(top, BorderLayout.NORTH);
   }
 
   public void displaySellableItem(ShopItem s, int x, int y){
@@ -49,8 +77,7 @@ public class Merchant extends JPanel  {
     box.setBounds(x,y,300,350);
     p.add(box);
 
-    p.setBounds(350*cnt++, 0, 350, 400);
-
+    p.setBounds(350*cnt, 0, 350, 400);
     this.add(p);
 }
   public void paintComponent(Graphics g){
